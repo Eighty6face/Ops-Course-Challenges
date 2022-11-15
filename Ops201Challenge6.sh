2477f97b656b013492782876b174afd29c54c780
Alex Wise <eighty6face@gmail.com>
2022-11-14 19:39:24 -0600

    Challenge 6

---
+++ Ops201Challenge6.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# create a script that detects a file or directory exsist, then creates it if it does not exsist
+# Must use one Array, one loop and one conditional
+fileContinue="y"
+while [ $fileContinue == "y" ]
+do
+    read -p "enter file or directory to be created" filecreate
+    if [[ -f $filecreate ]]
+    then
+    echo "$filecreate exsist on your machine"
+fi 
+if [[! -f $filecreate ]]
+then
+    echo "$filecreate does not exsist on your machine"
+fi
+
+if [[! -f $filecreate]]
+then
+    $mkdir $filecreate
+fi
+    read -p "any more file to create:" fileContinue
+done
