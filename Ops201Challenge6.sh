e3d4f9d11aa476a174c397834dbd13c89fe4caac
Alex Wise <116468261+Eighty6face@users.noreply.github.com>
2022-11-15 11:51:40 -0600

    Update Ops201Challenge6.sh

--- Ops201Challenge6.sh
+++ Ops201Challenge6.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
+# Script:                               Challenge 6                      
+# Author:                               Alex Wise           
+# Date of latest revision:              11-14-22      
+# Purpose:                              Practice 
+
+
+echo "does file.txt exist?"
+
+Xs=("f1.txt" "f2.txt" "f3.txt" "f4.txt" "f5.txt")
+
+for X in "${Xs[@]}"
+do
+    if [ -e ./$X ]
+    then 
+        echo yes
+    else
+        touch ./$X
+        echo "No, but $X has now been created"
+    fi
+done
 # create a script that detects a file or directory exsist, then creates it if it does not exsist
 # Must use one Array, one loop and one conditional
-fileContinue="y"
-while [ $fileContinue == "y" ]
-do
-    read -p "enter file or directory to be created" filecreate
-    if [[ -f $filecreate ]]
-    then
-    echo "$filecreate exsist on your machine"
-fi 
-if [[! -f $filecreate ]]
-then
-    echo "$filecreate does not exsist on your machine"
-fi
 
-if [[! -f $filecreate]]
-then
-    $mkdir $filecreate
-fi
-    read -p "any more file to create:" fileContinue
-done
-# extremely lost at this point as im getting errors i cant identify. Thought I had this one.
-# thanks to my accountability buddies for the teamwork and help
+#Thanks Grant for the help and walk through. SUPER CONFUSING TO ME
