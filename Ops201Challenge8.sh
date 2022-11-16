8a0ac46e72a4beaa0535958dfab3cd490cf0dee5
Alex Wise <116468261+Eighty6face@users.noreply.github.com>
2022-11-16 17:22:21 -0600

    Create Ops201Challenge8.sh
    Ops Challenge 8

---
+++ Ops201Challenge8.sh
@@ -0,0 +1,10 @@
+# Script:                       Batch Auto Save 
+# Author:                       Alex Wise
+# Date of latest revision:      11/16/2022
+# Purpose:                      Practice 
+
+robocopy "C:\Users\J.Thompson\Desktop\My Project Files" "Desktop"
+Pause
+
+# Paused to check from proper copying
+# E/: exsist but for somke reason gave my a sintax error 
