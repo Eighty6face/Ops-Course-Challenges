2bb56cce0f91f38fa468ed69e3666bccda9e1e6d
Alex Wise <116468261+Eighty6face@users.noreply.github.com>
2022-11-16 17:44:50 -0600

    Update Ops201Challenge8.sh

--- Ops201Challenge8.sh
+++ Ops201Challenge8.sh
@@ -3,8 +3,8 @@
 # Date of latest revision:      11/16/2022
 # Purpose:                      Practice 
 
-robocopy "C:\Users\J.Thompson\Desktop\My Project Files" "Desktop"
-Pause
+robocopy "C:\Users\j.thompson\Desktop\My Project Files" "E:/Batch"
+PAUSE
 
 # Paused to check from proper copying
 # E/: exsist but for somke reason gave my a sintax error 
