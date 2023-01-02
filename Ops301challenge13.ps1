5b253a24b76b0fb476904ce250577e8c302b439e
Alex Wise <116468261+Eighty6face@users.noreply.github.com>
2023-01-02 15:45:07 -0600

    Ops301challenge13.ps1

---
+++ Ops301challenge13.ps1
@@ -0,0 +1,8 @@
+:: .Ps1 File 
+
+:: Script:                    Ops 301 Challenge 13
+:: Author:                    Alex Wise  
+:: Date of latest revision:   12/29/2022  
+:: Purpose:                   PowerShell direcotry ques 
+
+New-ADUser -Name "Bart Simpson" -City "Oceanside" -State "California" -OtherAttributes @{'title'="TPS ReportingLead;'mail'="BSimpGlobeXpower.com"}
